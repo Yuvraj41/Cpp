e98d4a85a18d54b999115c9e750a22edb8fe9592
Yuvraj <yuvrajharsh41@gmail.com>
2024-06-05 11:12:04 +0530

    Counting

---
+++ functions/counting.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void count(int n)
+{
+    for(int i=1;i<=n;i++)
+    {
+        cout<< i <<" ";
+    }
+    cout<< endl;
+}
+   int main()
+   {
+    int n;
+    cin>>n;
+    count(n);
+    return 0;
+   }
